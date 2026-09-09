9de5bcc56dd9751d943b94c098dae5c4dd90147f
Developer Silo 👨‍💻 <97414850+siloneedsleep@users.noreply.github.com>
2026-09-09 20:39:47 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,21 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-inter)", "sans-serif"],
-        display: ["var(--font-space-grotesk)", "sans-serif"],
-      },
-      animation: {
-        marquee: "marquee 30s linear infinite",
-      },
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-50%)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
